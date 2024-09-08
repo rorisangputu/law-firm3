4e71fcd9f98af678e553b2d3b105ab3917069eb3
Rorisang Putu <rorisangputu@gmail.com>
2024-09-08 02:00:00 +0200

    changing lawyer order

--- src/Components/About/Partners.jsx
+++ src/Components/About/Partners.jsx
@@ -9,7 +9,7 @@ const Partners = () => {
                         <p className='text-[#830000]'>Your Partners in Legal Advocacy</p>
                         <h1 className='text-2xl'>Our Team of Legal Experts</h1>
                     </div>
-                    <div className='grid mg:grid-cols-2 lg:grid-cols-3 gap-7 pb-10'>
+                    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 pb-10'>
                         <div className='border'>
                             <img className='h-[45vh] object-left w-full object-cover' src="https://images.pexels.com/photos/5915239/pexels-photo-5915239.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
                             <div className='p-7 flex flex-col gap-3'>
